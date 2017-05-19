4d671f4dbfbfdf39a7dd954ca1524ac46871c945
qngapparat <jakob.wallnoefer@outlook.com>
2017-05-19 23:32:36 +0200

    rebuilt following tiny.cc/fhsktr

--- msg_recv.c
+++ msg_recv.c
@@ -1,45 +1,38 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
-#include <sys/stat.h>
+#include <sys/msg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX_BUF 128
 
+struct msgbuf {
+    long mtype;
+    char mtext[MAX_BUF];
+};
 
 int main(int argc, char const *argv[]) {
 
-    int msg,mask,msgid;
+    key_t key;
+    int msgid;
+    struct msgbuf rcvbuf;
 
-    mask = 0666;
-    key = ftok(".", "A");
-    msgid = msgget(key, mask); //returns error if doesnt exist
-
-    if(msgid == -1){ //in case msg doesn't exist, create one
-        printf("msgqueue doesn't exist. Creating one...\n");
-
-        //initialize
-        if((msgid = msgget(key, mask | IPC_CREAT)) == -1){
-            perror("msgget");
-            return EXIT_FAILURE;
-        }
-
-        int msg[2];
-        msg[1] = 1234;
-        msg[2] = 6789;
-
-        //send
-        if((int ret = msgsnd(msgid, msg, sizeof(int), IPC_NOWAIT)) == -1{
-            perror("msgsnd");
-            return EXIT_FAILURE;
-        }
-
+    if((msgid = msgget(key, 0666)) == -1){
+        perror("msgget");
+        return EXIT_SUCCESS;
     }
 
-    else{
-        printf("msgqueue exists already. cancelling\n");
+    //receive message  of type 1 (defined in struct msgbuf mtype in msg_send.c)
+    if(msgrcv(msgid, &rcvbuf, MAX_BUF, 1, 0) == -1){
+        perror("msgrcv");
         return EXIT_FAILURE;
     }
 
-    printf("completed. terminating\n");
+    else{
+        printf("message received:\n");
+        printf("%s\n", rcvbuf.mtext);
+    }
+
+
     return EXIT_SUCCESS;
 }
---
+++ msg_send.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/stat.h>
+#include <sys/msg.h>
+#include <string.h>
+
+#define MAX_BUF 128
+
+struct msgbuf {
+    long mtype;
+    char mtext[MAX_BUF];
+};
+
+
+int main(int argc, char const *argv[]) {
+
+    int msgid;
+    key_t key;
+
+    struct msgbuf sndbuf;
+    sndbuf.mtype = 1;
+
+    size_t buflen;
+    int mask = 0666 | IPC_CREAT;
+
+    key = 1234;
+
+    if((msgid = msgget(key, mask))== -1){ //in case msg doesn't exist, create one
+        perror("msgget");
+        return EXIT_FAILURE;
+    }
+
+    printf("enter a text:\n");
+    scanf("%[^\n]",sndbuf.mtext);
+    getchar(); //NOTE for whatever reason
+
+    buflen = strlen(sndbuf.mtext) + 1;
+
+    if((msgsnd(msgid, &sndbuf, buflen, IPC_NOWAIT)) == -1){
+        perror("msgsnd");
+        return EXIT_FAILURE;
+    }
+
+    else{
+        printf("message sent\n");
+    }
+
+    printf("completed. terminating\n");
+    return EXIT_SUCCESS;
+}
