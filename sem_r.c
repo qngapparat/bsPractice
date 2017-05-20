d28caafe563ebe410c66ea02cdeb20718f74488b
qngapparat <jakob.wallnoefer@outlook.com>
2017-05-20 20:24:08 +0200

    semaphores working.

--- sem_r.c
+++ sem_r.c
@@ -18,22 +18,22 @@ int main(int argc, char const *argv[]) {
     watch((semid = semget(key, 1, mask)), "semget");
 
     //create lock and unlock structs
-    struct sembuf lock;
-    lock.sem_num = 0; //semaphore #
-    lock.sem_op = -1; //decrement semaphore to lock
-    lock.sem_flg = 0; //waits in case semaphore is 0
+    struct sembuf lock[1];
+    lock[0].sem_num = 0; //semaphore #
+    lock[0].sem_op = -1; //decrement semaphore to lock
+    lock[0].sem_flg = SEM_UNDO; //waits in case semaphore is 0
 
-    struct sembuf unlock; //TODO to array
-    unlock.sem_num = 0;
-    unlock.sem_op = +1;
-    unlock.sem_flg = 0; //TODO might change this
+    struct sembuf unlock[1]; //TODO to array
+    unlock[0].sem_num = 0;
+    unlock[0].sem_op = +1;
+    unlock[0].sem_flg = SEM_UNDO; //TODO might change this
 
 
     printf("waiting for critical region\n");
 
-    watch((semop(semid, &lock, 1)), "semop");
+    watch((semop(semid, lock, 1)), "semop");
     printf("im in the critical region man\n");
-    watch((semop(semid, &unlock, 1)), "semop");
+    watch((semop(semid, unlock, 1)), "semop");
 
     printf("done. terminating\n");
     return EXIT_SUCCESS;
--- sem_w.c
+++ sem_w.c
@@ -7,6 +7,12 @@
 #include <sys/types.h>
 #include "error_handler.h"
 
+union semun {
+    int val;/* value of SETVAL */
+    struct semid_ds *buf;/* buffer for IPC_STAT,IPC_SET */
+    ushort *array;/* array for GETALL, SETALL */
+};
+
 int main(int argc, char const *argv[]) {
 
     int mask = IPC_CREAT | 0666;
@@ -17,33 +23,45 @@ int main(int argc, char const *argv[]) {
     watch((semid = semget(key, 1, mask)), "semget");
 
     //create lock and unlock structs
-    struct sembuf lock;
-    lock.sem_num = 0; //semaphore #
-    lock.sem_op = -1; //decrement semaphore to lock
-    lock.sem_flg = 0; //waits in case semaphore is 0
+    struct sembuf lock[1];
+    lock[0].sem_num = 0; //semaphore #
+    lock[0].sem_op = -1; //decrement semaphore to lock
+    lock[0].sem_flg = SEM_UNDO; //waits in case semaphore is 0
 
-    struct sembuf unlock; //TODO to array
-    unlock.sem_num = 0;
-    unlock.sem_op = +1;
-    unlock.sem_flg = 0; //TODO might change this
+    struct sembuf unlock[1]; //TODO to array
+    unlock[0].sem_num = 0;
+    unlock[0].sem_op = +1;
+    unlock[0].sem_flg = SEM_UNDO; //TODO might change this
 
 
     //this way, semop can replace the initial semctl (at least with one semaphore).
-    lock.sem_op = 1;
-    watch((semop(semid, &lock, 1)), "initial semop (semctl replacement)");
+    /*lock.sem_op = 1;
+    watch((semop(semid, lock, 1)), "initial semop (semctl replacement)");
 
     //reset our operation
-    lock.sem_op = +1;
+    lock.sem_op = +1;*/
 
-    watch((semop(semid, &lock, 1)), "semop");
+    union semun arg;
+    unsigned short values[1];
+    values[0] = 1;
+    arg.array = values;
+
+    watch((semctl(semid, 0, SETALL, arg)), "init setctl");
+
+
+    watch((semop(semid, lock, 1)), "semop");
     printf("im in critical region... sleeping here for 10 sec\n");
-
     int i = 0;
     while(i++ < 10){
         sleep(1);
     }
+    watch((semop(semid, unlock, 1)), "semop");
 
-    watch((semop(semid, &unlock, 1)), "semop");
+    printf("waiting 5 sec for reader to finish before terminating\n");
+    //Waiting 5 seconds. using i=10 from above.
+    while(i-- > 5){
+        sleep(1);
+    }
 
     printf("unlinking + terminating\n");
     watch((semctl(semid, 0, IPC_RMID)), "semctl");
