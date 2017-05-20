c85d590f7d2fbea6634953577fc698d655a1533d
qngapparat <jakob.wallnoefer@outlook.com>
2017-05-20 16:11:20 +0200

    corrected file names

--- shm_rcv.c
+++ shm_rcv.c
@@ -9,43 +9,31 @@ int main(int argc, char const *argv[]) {
 
     key_t key;
     int shmid;
-    char c, *shm, *s;
+    char *s, *shm;
 
-    int mask = 0666;
     const int MAX_SIZE = 128;
+    int mask = IPC_CREAT | 0666;
     key = ftok(".", 'a');
 
     if((shmid = shmget(key, MAX_SIZE, mask)) == -1){
-        perror("shmget");
+        perror("shmget")
         return EXIT_FAILURE;
     }
 
-    if((shm = shmat(shmid, NULL, 0)) == (char*)-1 ){
+    if((shm = shmat(shmid, NULL, 0)) == (char*) -1){
         perror("shmat");
-        return EXIT_FAILURE;
+        return EXIT_SUCCESS;
     }
 
-    //write to memory
-    //dont change orginal pointer to not loose value
-    s = shm;
-    for(c = 'a', c < 'z', c++){
-        s* = c;
-        s++;
+
+    for(s = shm; *s != NULL; s++){
+        putchar(*s);
     }
 
-    //sleep until first char is set to *. //TODO exit immediately without shmdt. does sender still work?
-    
-    while((*shm) != '*'){
-        sleep(1);
-    }
+    putchar('\n');
 
-    //detach segment
-    if((shmdt(shm)) == -1){
-        perror("shmdt");
-        return EXIT_FAILURE;
-    }
-
-    printf("detached. terminating\n");
+    //set complete "flag"
+    *shm = '*';
 
     return EXIT_SUCCESS;
 }
--- shm_snd.c
+++ shm_snd.c
@@ -9,22 +9,42 @@ int main(int argc, char const *argv[]) {
 
     key_t key;
     int shmid;
+    char c, *shm, *s;
 
+    int mask = 0666;
     const int MAX_SIZE = 128;
-    int mask = IPC_CREAT | 0666;
     key = ftok(".", 'a');
 
     if((shmid = shmget(key, MAX_SIZE, mask)) == -1){
-        perror("shmget")
+        perror("shmget");
         return EXIT_FAILURE;
     }
 
-    if((shmat(shmid, NULL, 0)) == (char*) -1){
+    if((shm = shmat(shmid, NULL, 0)) == (char*)-1 ){
         perror("shmat");
-        return EXIT_SUCCESS;
+        return EXIT_FAILURE;
     }
 
+    //write to memory
+    //dont change orginal pointer to not loose value
+    s = shm;
+    for(c = 'a', c < 'z', c++){
+        s* = c;
+        s++;
+    }
 
+    //sleep until first char is set to *. //TODO exit immediately without shmdt. does sender still work?
+    while((*shm) != '*'){
+        sleep(1);
+    }
+
+    //detach segment
+    if((shmdt(shm)) == -1){
+        perror("shmdt");
+        return EXIT_FAILURE;
+    }
+
+    printf("detached. terminating\n");
 
     return EXIT_SUCCESS;
 }
