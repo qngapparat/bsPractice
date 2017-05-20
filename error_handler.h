b6abf2b3f5ceb8c93677ca8d84222ffabf8b1505
qngapparat <jakob.wallnoefer@outlook.com>
2017-05-20 15:39:25 +0200

    added die() error handler

---
+++ error_handler.h
@@ -0,0 +1,6 @@
+#ifndef ERROR_HANDLER_
+#define ERROR_HANDLER_
+
+void die(char* msg);
+
+#endif
--- shm_snd.c
+++ shm_snd.c
@@ -14,8 +14,15 @@ int main(int argc, char const *argv[]) {
     int mask = IPC_CREAT | 0666;
     key = ftok(".", 'a');
 
+    if((shmid = shmget(key, MAX_SIZE, mask)) == -1){
+        perror("shmget")
+        return EXIT_FAILURE;
+    }
 
-
+    if((shmat(shmid, NULL, 0)) == (char*) -1){
+        perror("shmat");
+        return EXIT_SUCCESS;
+    }
 
 
 
