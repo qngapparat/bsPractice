b8c6654cb48c148138a9b05d5b523f557e3345cd
qngapparat <jakob.wallnoefer@outlook.com>
2017-05-22 23:23:07 +0200

    added sample task for demonstration

--- queue.c
+++ queue.c
@@ -4,47 +4,62 @@
 #include <stdbool.h>
 #include <time.h>
 
-#define LENGTH 16
+#define LENGTH 8
 
 int queue[LENGTH];
 
 int put_ptr, take_ptr = 0;
 bool empty = true;
-
+bool full = false;
 
 bool enqueue(int value){
     //means that queue is fill. Empty is switched to false by the first ever enqueue, to distinguish between the initial ptr position and both pointers randomly meeting at zero.
-
-    if(put_ptr == take_ptr && !empty){
+    if(full){
+        printf("enqueue: queue full\n");
         return false;
     }
 
-    else{ //insert into circular array
-        queue[put_ptr] = value;
-        put_ptr = (put_ptr + 1) % LENGTH;
-        empty = false;
+    if(((put_ptr + 1) % LENGTH) == take_ptr){
+        //next insert will render queue full
+        full = true;
     }
+
+     //insert into circular array
+    queue[put_ptr] = value;
+    put_ptr = (put_ptr + 1) % LENGTH;
+    empty = false;
+    printf("enqueued: %d\n", value);
+    return true;
 }
 
 int top(){
     if(!empty){
         return queue[take_ptr];
     }
+
+    else{
+        printf("top: queue empty. terminating\n");
+        exit(EXIT_FAILURE);
+    }
 }
 
 
-int dequeue(){
+bool dequeue(){
 
-    if(take_ptr == put_ptr){
-        printf("queue empty. couldnt dequeue\n");
-        return EXIT_FAILURE; //not the most elegant solution
-    }
-    else{
-        int temp = queue[take_ptr];
-        take_ptr = (take_ptr + 1) % LENGTH;
-        return temp;
+    if(empty){
+        printf("dequeue: queue empty\n");
+        return false;
     }
 
+    if(((take_ptr + 1) % LENGTH) == put_ptr){
+        //this dequeue will render the queue empty
+        empty = true;
+    }
+
+    int temp = queue[take_ptr];
+    take_ptr = (take_ptr + 1) % LENGTH;
+    full = false;
+    return true;
 }
 
 int main(int argc, char const *argv[]) {
@@ -55,13 +70,18 @@ int main(int argc, char const *argv[]) {
     printf("filling queue...\n");
     while(enqueue(rand()%20)){ }
 
-    printf("top: %d\n", top());
-    printf("dequeue: %d\n", dequeue());
-    printf("top: %d\n", top());
-    printf("dequeue: %d\n", dequeue());
-    printf("top: %d\n", top());
+    //dequeue one and enqueue one
+    printf("%d\n", top());
+    dequeue();
+    enqueue(183);
 
-    printf("done\n");
+    //print and dequeue until empty
+    while(1){
+        printf("%d\n", top());
+        dequeue();
+    }
+
+    printf("\ndone\n");
 
 
     return EXIT_SUCCESS;
