030366e6101e04bb9a8efdbd793f63fed269da2d
qngapparat <jakob.wallnoefer@outlook.com>
2017-05-23 20:49:03 +0200

    added bubbleup and bubbledown

--- heap_array.c
+++ heap_array.c
@@ -2,18 +2,35 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-bool initialized = false;
+//NOTE flooring integers is towards 0 is needed in bubble_up(). C99 and up automatically floors towards zero, so this requires no additional functions.
 
-void clear_element(int my_heap, int index){
+//using this to avoid Indexoutofbounds exceptions, eg accessing array with invalid index.
+bool can_have_left_child(int my_heap, int index){
+
+    if((2*index) <= my_heap[0] && index != 0){
+        return true;
+    }
+
+    return false;
+}
+
+bool can_have_right_child(int my_heap, int index){
+
+    if((2*index+1) <= my_heap[0] && index != 0){
+        return true;
+    }
+
+    return false;
+}
+
+int clear_element(int my_heap, int index){
+
     my_heap[index] = -1;
-    return;
+    return my_heap;
 }
 
 int find_last_element(int my_heap){
-    if(!initialized){
-        printf("removeMin: Heap not initialized.");
-        exit(EXIT_FAILURE);
-    }
+
 
     //get how many elements are stored
     int length = my_heap[0];
@@ -21,32 +38,70 @@ int find_last_element(int my_heap){
     return my_heap[length];
 }
 
-bool bubble_down(int my_heap){
+bool bubble_down(int my_heap, int index){
 
+/*When parent is located at index n , the left child is located at index 2n, and the right child at 2n + 1. (Assuming the root is located at index 1 of the array, not 0)*/
+
+    if(can_have_left_child(my_heap, index) && (my_heap[index] > my_heap[2*index]) ){ //if parent is bigger than left child.
+        int temp = my_heap[index];
+        my_heap[index] = my_heap[2*index];
+        my_heap[2*index] = temp;
+
+        bubble_down(my_heap, 2*index); //bubble down left subtree
+    }
+
+    if(can_have_right_child(my_heap, index) && (my_heap[index] > my_heap[(2*index) + 1])){ //if parent is bigger than right child.
+        int temp = my_heap[index];
+        my_heap[index] = my_heap[(2*index) + 1];
+        my_heap[(2*index) + 1] = temp;
+
+        bubble_down(my_heap, (2*index+1)); //bubble down right subtree
+    }
+
+    //TODO can you ensure it was successful at this pt?
+    return true;
 }
 
 int removeMin(int my_heap){
 
-    if(!initialized){
-        printf("removeMin: Heap not initialized.");
-        exit(EXIT_FAILURE);
-    }
-
     int root  = my_heap[1];
     my_heap[1] = find_last_element(my_heap);
-    clear_element(my_heap, my_heap[0]);
+    my_heap = clear_element(my_heap, my_heap[0]);
     bubble_down(my_heap);
 
 
 }
 
-bool bubble_up(int my_heap){
+bool bubble_up(int my_heap, int index){
 
+    if(index == 1){
+        return true;
+    }
+
+    //check if parent is bigger
+    if(my_heap[index/2] > my_heap[index]){
+
+        //swap
+        int temp = my_heap[index];
+        my_heap[index] = my_heap[index/2];
+        my_heap[index/2] = temp;
+
+        //bubble up from parent position
+        return bubble_up(my_heap, (index/2))
+    }
+
+    else{
+        return true;
+    }
 
 }
 
 bool insert(int my_heap, int value){
 
+    int free_index = my_heap[0] + 1;
+    my_heap[free_index] = value;
+
+    return bubble_up(my_heap, free_index);
 
 }
 
